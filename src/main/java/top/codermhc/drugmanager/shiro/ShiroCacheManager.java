01bada8d3d6a602f56d66f98eb41141224fc2c13
YeMinghui <MH_C_Y@163.com>
2020-05-11 19:13:29 +0800

    删除多余依赖，修改Shiro缓存前缀

--- src/main/java/top/codermhc/drugmanager/shiro/ShiroCacheManager.java
+++ src/main/java/top/codermhc/drugmanager/shiro/ShiroCacheManager.java
@@ -7,19 +7,21 @@ import org.apache.shiro.cache.CacheException;
 import org.apache.shiro.cache.CacheManager;
 import org.springframework.data.redis.core.RedisTemplate;
 
+@SuppressWarnings("rawtypes")
 public class ShiroCacheManager implements CacheManager {
 
-    private String prefix = "shiro:";
+    private final String prefix = "shiro.";
     private final ConcurrentHashMap<String, Cache> caches = new ConcurrentHashMap<>();
 
     @Resource(name = "shiroRedisTemplate")
     private RedisTemplate<String, Object> redisTemplate;
 
     @Override
-    public <K, V> Cache<K, V> getCache(String name) throws CacheException {
+    @SuppressWarnings("unchecked")
+    public Cache getCache(String name) throws CacheException {
         Cache cache = caches.get(name);
         if (cache == null) {
-            cache = new ShiroRedisCache<>(prefix + name, redisTemplate);
+            cache = new ShiroRedisCache(prefix + name, redisTemplate);
             caches.put(name, cache);
         }
         return cache;
